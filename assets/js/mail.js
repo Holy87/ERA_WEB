166d83fb934f3ff46654dac6759eb6fa1ea09da6
Holy87 <fr.bosso@outlook.it>
2017-10-23 20:46:52 +0200

    Aggiunta funzione invio messaggi Rimozione link elements Aggiustatine

---
+++ assets/js/mail.js
@@ -0,0 +1,29 @@
+function send_message(e) {
+    var button = $("#submitter");
+    button.prop('disabled', "diabled");
+    button.html('<i class="fa fa-circle-o-notch fa-spin fa-fw"></i> Invio in corso...');
+    $.ajax({
+        type: 'post',
+        url: '/messaggio.php',
+        dataType: 'json',
+        data: $("#messageForm").serialize(),
+        success: function (response) {
+            if (response.ok)
+                alert("Messaggio inviato correttamente");
+            else
+                alert("C'è stato un errore nell'invio del messaggio. Per favore, riprova");
+            button.html('Invia il messaggio');
+            button.removeAttr('disabled');
+        }
+
+        }
+    );
+    e.preventDefault();
+}
+
+$(document).ready(function() {
+    $('#messageForm').submit(function (e) {
+        send_message(e);
+    })
+
+});
--- messaggio.php
+++ messaggio.php
@@ -1,8 +1,8 @@
 <?php
 $to = "info@eraascensori.it";
-$from = $_POST["mail"];
-$name = $_POST["nome"];
-$message = $_POST["messaggio"];
+$from = $_POST["email"];
+$name = $_POST["name"];
+$message = $_POST["message"];
 $subj_text = 'Hai un nuovo messaggio da un visitatore';
 
 $message.= "\n\r *Questo messaggio è stato inviato usando il form di contatto dal sito web.";
@@ -12,8 +12,9 @@ $headers .= "Content-type:text/html;charset=UTF-8 \r\n";
 $headers .= "From: ".$name." <sito@eraascensori.it> \r\n";
 $headers .= "Reply-To: ".$name." <".$from."> \r\n";
 
+
 if(mail($to,$subj_text,$message,$headers)) {
-    echo "ok";
+    echo json_encode(['ok' => true]);
 } else {
-    echo "error";
+    echo json_encode(['ok' => false]);
 }

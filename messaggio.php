9824ce304b9d048f54bc38dde3788519e08f4ef6
Holy87 <fr.bosso@outlook.it>
2017-10-28 20:23:54 +0200

    Aggiustati link social Aggiustata mappa responsive

--- messaggio.php
+++ messaggio.php
@@ -5,14 +5,20 @@ $name = $_POST["name"];
 $message = $_POST["message"];
 $subj_text = 'Hai un nuovo messaggio da un visitatore';
 
-$message.= "\n\r *Questo messaggio è stato inviato usando il form di contatto dal sito web.";
+$signature = "*Questo messaggio è stato inviato usando il form di contatto dal sito web.";
 
+$body = "<html><body>
+<h1>Messaggio:</h1>
+<p>$message</p>
+<hr>
+<p>$signature</p>
+</body></html>";
 $headers = "MIME-Version: 1.0 \r\n";
 $headers .= "Content-type:text/html;charset=UTF-8 \r\n";
-$headers .= "From: ".$name." <sito@eraascensori.it> \r\n";
-$headers .= "Reply-To: ".$name." <".$from."> \r\n";
+$headers .= "From: $name <sito@eraascensori.it> \r\n";
+$headers .= "Reply-To: ".$name." <$from> \r\n";
 
-if(mail($to,$subj_text,$message,$headers)) {
+if(mail($to,$subj_text,$body,$headers)) {
     $outp = json_encode(['ok' => true]);
 } else {
     $outp = json_encode(['ok' => false]);

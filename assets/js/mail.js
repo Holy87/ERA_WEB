b32fe0e761e1edc8eaf784e2f3f55e4d83b713f0
Holy87 <fr.bosso@outlook.it>
2017-10-29 20:07:31 +0100

    aggiustatine

--- assets/js/mail.js
+++ assets/js/mail.js
@@ -2,10 +2,12 @@ function send_message(e) {
     var button = $("#submitter");
     button.prop('disabled', "diabled");
    //button.html('<i class="fa fa-circle-o-notch fa-spin fa-fw"></i> Invio in corso...');
+    console.log($("#messageForm").serialize());
     $.ajax({
         type: 'post',
         url: 'http://www.francescobosso.altervista.org/era/messaggio.php',
         dataType: 'jsonp',
+        crossDomain: true,
         jsonpCallback: 'result',
         data: $("#messageForm").serialize(),
         success: function (response) {

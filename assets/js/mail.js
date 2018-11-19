041e1d1cd1070cf8db2a17103fd7c037dbf202fc
Holy87 <4venger!>
2018-11-20 00:31:34 +0100

    Sistemazioni varie e ritorno ad utilizzare il PHP per la mail

--- assets/js/mail.js
+++ assets/js/mail.js
@@ -1,15 +1,12 @@
 function send_message(e) {
-    var button = $("#submitter");
+    let button = $("#submitter");
     button.prop('disabled', "diabled");
-   //button.html('<i class="fa fa-circle-o-notch fa-spin fa-fw"></i> Invio in corso...');
-    console.log('http://www.francescobosso.altervista.org/era/messaggio.php?'+$("#messageForm").serialize());
+    //button.html('<i class="fa fa-circle-o-notch fa-spin fa-fw"></i> Invio in corso...');
     $.ajax({
         type: 'post',
-        url: 'http://www.francescobosso.altervista.org/era/messaggio.php?'+$("#messageForm").serialize(),
-        dataType: 'jsonp',
-        crossDomain: true,
-        jsonpCallback: 'result',
-        //data: $("#messageForm").serialize(),
+        url: 'messaggio.php',
+        dataType: 'json',
+        data: $("#messageForm").serialize(),
         success: function (response) {
             if (response.ok)
                 alert("Messaggio inviato correttamente");
